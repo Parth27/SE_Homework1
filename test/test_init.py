0d24de2af91726a71d8c2d52631aa3cb4e15538a
Parth Diwanji <diwanji.parth@gmail.com>
2020-08-17 23:41:55 -0400

    Update test_init.py

--- test/test_init.py
+++ test/test_init.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 import os
 import sys
-sys.path.append(os.path.join(os.path.abspath('..'),'code'))
-from main import arith_sum
+from code.main import arith_sum
 
 def test_arith_sum():
     assert arith_sum(2,3) == 5
